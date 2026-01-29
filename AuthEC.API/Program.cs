b86fd2566c9d247465470f2e8786b16c5cf6c82c
Sandeep Kumar <60171858+SandeepKumar377@users.noreply.github.com>
2026-01-30 01:40:26 +0530

    Added Identity framework service

--- AuthEC.API/Program.cs
+++ AuthEC.API/Program.cs
@@ -1,8 +1,17 @@
+using AuthEC.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddDbContext<AppDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
